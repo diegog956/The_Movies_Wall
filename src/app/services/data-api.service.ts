1f7cc8c7fff1a1f6a1e4adb8787d167ed6bff60a
Diego <diego.gerardo95@hotmail.com>
2023-11-19 10:35:57 -0300

    Services

---
+++ src/app/services/data-api.service.ts
@@ -0,0 +1,68 @@
+
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpHeaders  } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class DataAPIService {
+
+  arreglo: any[] = [1, 2, 3];
+
+  constructor(private http: HttpClient) { }
+
+  getData(url: string): Observable<any> {
+
+    const headers = {
+      'X-RapidAPI-Key': '926e6df793msh0ea9d04f76400bdp11e7f3jsn10abb3666698',
+      'X-RapidAPI-Host': 'moviesdatabase.p.rapidapi.com'
+    };
+
+
+    return this.http.get<any>(url, { headers });
+  }
+
+  translateText(text: string): Observable<any> {
+    const url = 'https://rapid-translate-multi-traduction.p.rapidapi.com/t';
+    const body = {
+      from: 'en',
+      to: 'es',
+      q: text
+    };
+    const headers = new HttpHeaders({
+      'content-type': 'application/json',
+      'X-RapidAPI-Key': '926e6df793msh0ea9d04f76400bdp11e7f3jsn10abb3666698',
+      'X-RapidAPI-Host': 'rapid-translate-multi-traduction.p.rapidapi.com'
+    });
+
+    return this.http.post<any>(url, body, { headers });
+  }
+
+
+  getSearchData(url: string): Observable<any> {
+
+    const headers = {
+      'X-RapidAPI-Key': '926e6df793msh0ea9d04f76400bdp11e7f3jsn10abb3666698',
+      'X-RapidAPI-Host': 'moviesdatabase.p.rapidapi.com'
+    };
+
+    const params = {
+    exact: 'false',
+    info: 'base_info',
+    page: '1',
+    //sort: 'pos.incr',
+    endYear: '2022',
+    titleType: 'movie',
+    limit: '21',
+    list: 'top_boxoffice_200'
+    };
+
+    
+    return this.http.get<any>(url, {params, headers });
+  }
+
+
+}
+
---
+++ src/app/services/users-database.service.ts
@@ -0,0 +1,359 @@
+import { Injectable } from '@angular/core';
+import { User } from '../interfaces/User';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { Movie } from '../interfaces/Movie';
+import { count } from '@swimlane/ngx-charts';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsersDatabaseService {
+
+  constructor(private router: Router, private httpClient: HttpClient) { }
+
+  url: string = 'http://localhost:4000/Users/?timestamp=' + new Date().getTime();
+  auth: boolean = false;
+
+
+  async add(user: User) {
+
+    try {
+      await fetch(this.url, {
+        method: 'POST',
+        body: JSON.stringify(user),
+        headers: { 'content-type': 'application/json' }
+      })
+
+      this.router.navigate(['Home']);
+
+    } catch (error) {
+
+      console.log(error);
+
+    }
+
+  }
+
+  async getUsers(): Promise<User[] | undefined> {
+
+    try {
+      const result = await fetch(this.url);
+      const Users = result.json();
+      return Users;
+
+    } catch (error) {
+
+      console.log(error);
+      return undefined;
+
+    }
+
+
+  }
+
+  async authentication(email: string, password: string): Promise<boolean | undefined> {
+
+    try {
+      const users = await this.getUsers();
+
+
+
+      if (users) {
+        for (let user of users) {
+
+          if (user.Email === email && user.Password === password) {
+            this.auth = true;
+            return true;
+          }
+        }
+        this.auth = false;
+        return false;
+      } else {
+        return undefined;
+      }
+    }
+    catch (error) {
+      console.log(error);
+      return undefined;
+    }
+
+
+  }
+
+
+  isAuthenticated() {
+    return this.auth;
+  }
+
+
+
+  async getStats(url: string): Promise<any[] | undefined> {
+
+    try {
+      const result = await fetch(url);
+      const Stats = result.json();
+      return Stats;
+
+    } catch (error) {
+
+      console.log(error);
+      return undefined;
+
+    }
+
+
+  }
+
+  async getUser(email: string): Promise<User | undefined> {
+
+    try {
+      const users = await this.getUsers();
+
+
+      if (users) {
+        for (let user of users) {
+
+          if (email == user.Email) {
+
+            return user;
+
+          }
+        }
+      }
+      return undefined
+    }
+    catch (error) {
+      console.log(error);
+      return undefined;
+    }
+  }
+
+  async getUserByID(id: string): Promise<User | undefined> {
+
+    try {
+      const users = await this.getUsers();
+
+
+      if (users) {
+        for (let user of users) {
+
+          if (id == user.id.toString()) {
+
+            return user;
+
+          }
+        }
+      }
+      return undefined
+    }
+    catch (error) {
+      console.log(error);
+      return undefined;
+    }
+  }
+
+
+  async addFavMovieToUser(id: string, movie: Movie) {
+
+    try {
+      const users = await this.getUsers();
+
+      if (users) {
+        for (let user of users) {
+
+          if (id == user.id.toString()) {
+
+            user.FavMovies.push(movie);
+
+            try {
+
+              await fetch('http://localhost:4000/Users/' + id, {
+                method: 'PUT',
+                body: JSON.stringify(user),
+                headers: { 'content-type': 'application/json' }
+              })
+              console.log(user.FavMovies);
+              console.log(user.id);
+
+              return true;
+            } catch (error) {
+
+              console.log(error);
+
+              return false;
+
+            }
+          }
+        }
+        return false;
+      }
+      else {
+        return undefined
+      }
+    }
+    catch (error) {
+      console.log(error);
+      return undefined;
+    }
+
+  }
+
+
+  async removeMovieFromFavorites(id: string, idmovie: String) {
+
+    const user = await this.getUserByID(id);
+
+    if (user) {
+
+      let peliculaAEliminar = user.FavMovies.find(pelicula => pelicula.id == idmovie);
+
+      if (peliculaAEliminar) {
+        user.FavMovies = user.FavMovies.filter(pelicula => pelicula !== peliculaAEliminar);
+        console.log(user.FavMovies);
+
+        try {
+
+          await fetch('http://localhost:4000/Users/' + id, {
+            method: 'PUT',
+            body: JSON.stringify(user),
+            headers: { 'content-type': 'application/json' }
+          })
+        }
+        catch (error) {
+          console.log(error);
+          return undefined;
+        }
+
+      }
+      else {
+        console.log('La película no fue encontrada.');
+      }
+    }
+  }
+
+
+
+
+
+  async MovieSearch(id: string, idmovie: string): Promise<boolean | undefined> {
+
+    const user = await this.getUserByID(id)
+
+
+    if (user) {
+      for (let movie of user.FavMovies) {
+
+        if (movie.id == idmovie) {
+
+          return true;
+
+        }
+      }
+      return false;
+    }
+    else {
+      return undefined;
+    }
+  }
+
+
+  async addCountry(country: string) {
+
+    const stats = await this.getStats('http://localhost:4000/Countries');
+
+    if (stats != undefined) {
+      const countryIndex = stats.findIndex(c => c.id === country);
+
+      if (countryIndex !== -1) {
+
+        stats[countryIndex].value += 1;
+
+
+      }
+
+      try {
+
+        await fetch('http://localhost:4000/Countries/' + country, {
+          method: 'PUT',
+          body: JSON.stringify(stats[countryIndex]),
+          headers: { 'content-type': 'application/json' }
+        })
+      }
+      catch (error) {
+        console.log(error);
+        return undefined;
+      }
+
+
+    }
+  }
+
+
+  async addGenre(genres: any[]) {
+
+    const stats = await this.getStats('http://localhost:4000/Genres');
+
+    if (stats) {
+
+      for (let genre of genres) {
+
+        const genreIndex = stats.findIndex(c => c.id === genre.id);
+
+        if (genreIndex !== -1) {
+
+          stats[genreIndex].value += 1;
+
+
+          try {
+
+            await fetch('http://localhost:4000/Genres/' + genre.id, {
+              method: 'PUT',
+              body: JSON.stringify(stats[genreIndex]),
+              headers: { 'content-type': 'application/json' }
+            })
+          }
+          catch (error) {
+            console.log(error);
+            return undefined;
+          }
+        }
+      }
+    }
+  }
+
+  async removeGenre(genres: any[]) {
+
+    const stats = await this.getStats('http://localhost:4000/Genres');
+
+    if (stats) {
+
+      for (let genre of genres) {
+
+        const genreIndex = stats.findIndex(c => c.id === genre.id);
+
+        if (genreIndex !== -1) {
+
+          stats[genreIndex].value -= 1;
+
+
+          try {
+
+            await fetch('http://localhost:4000/Genres/' + genre.id, {
+              method: 'PUT',
+              body: JSON.stringify(stats[genreIndex]),
+              headers: { 'content-type': 'application/json' }
+            })
+          }
+          catch (error) {
+            console.log(error);
+            return undefined;
+          }
+        }
+      }
+    }
+  }
+}
+
+
