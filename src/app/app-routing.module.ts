addf201925009c05245b320eec687bae34447463
Diego <diego.gerardo95@hotmail.com>
2023-11-19 10:37:03 -0300

    SrcApp

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { LogInPageComponent } from './pages/log-in-page/log-in-page.component';
+import { MoviePageComponent } from './pages/movie-page/movie-page.component';
+import { SignUpPageComponent } from './pages/sign-up-page/sign-up-page.component';
+import { LogInAccessGuard } from './guards/log-in-access.guard';
+import { AdminPageComponent } from './pages/admin-page/admin-page/admin-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'Home', component: HomePageComponent, canActivate:[LogInAccessGuard]},
+  {path: 'LogIn', component: LogInPageComponent},
+  {path: 'Movie-id/:id', component: MoviePageComponent,canActivate:[LogInAccessGuard]},
+  {path: 'SignUp', component: SignUpPageComponent},
+  {path: 'Admin', component: AdminPageComponent, canActivate:[LogInAccessGuard]},
+  {path: '**', redirectTo:'LogIn'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,40 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { MoviePageComponent } from './pages/movie-page/movie-page.component';
+import { LogInPageComponent } from './pages/log-in-page/log-in-page.component';
+import { HttpClientModule } from '@angular/common/http';
+import { NgOptimizedImage } from '@angular/common';
+import { SignUpPageComponent } from './pages/sign-up-page/sign-up-page.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { AdminPageComponent } from './pages/admin-page/admin-page/admin-page.component';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    MoviePageComponent,
+    LogInPageComponent,
+    SignUpPageComponent,
+    AdminPageComponent
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    NgOptimizedImage,
+    ReactiveFormsModule,
+    NgxChartsModule,
+    BrowserAnimationsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
