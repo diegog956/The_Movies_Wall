36e4ca3c7b8b56f5bd0b46af04d613e2e929031a
Diego <diego.gerardo95@hotmail.com>
2023-11-19 10:35:07 -0300

    Log In Page

---
+++ src/app/pages/log-in-page/log-in-page.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { local } from 'd3-selection';
+import { UsersDatabaseService } from 'src/app/services/users-database.service';
+
+@Component({
+  selector: 'app-log-in-page',
+  templateUrl: './log-in-page.component.html',
+  styleUrls: ['./log-in-page.component.css']
+})
+export class LogInPageComponent implements OnInit {
+
+  title: string = '¡Bienvenido!'
+  public formLogin!: FormGroup;
+
+  email!: string;
+  password!: string;
+
+  constructor(private formBuilder: FormBuilder, private userDataService: UsersDatabaseService, private router: Router) { }
+  ngOnInit(): void {
+    this.formLogin = this.formBuilder.group({
+
+      Email: ['medina@gmail.com', [Validators.required]],
+      Password: ['cristianmedina10', [Validators.required]],
+
+    })
+  }
+
+  async login() {
+
+    this.email = this.formLogin.controls['Email'].value;
+    this.password = this.formLogin.controls['Password'].value;
+
+    if (await this.userDataService.authentication(this.email, this.password)) {
+      localStorage.setItem('isLoggedIn', 'true');
+      localStorage.setItem('userEmail', this.email);
+
+      const user = await this.userDataService.getUser(this.email);
+      if(user){
+      localStorage.setItem('id', user.id.toString());
+      localStorage.setItem('name', user.Name);
+      }
+      this.router.navigate(['/Home']);
+    } else {
+
+      alert("Email y/o contraseña incorrecto");
+    }
+  }
+
+  
+}
