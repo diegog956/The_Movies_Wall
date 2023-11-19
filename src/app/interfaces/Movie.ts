f40a5eab5c22a7585a515e8fc56311f2732a9256
Diego <diego.gerardo95@hotmail.com>
2023-11-19 10:34:08 -0300

    Interfaces

---
+++ src/app/interfaces/Movie.ts
@@ -0,0 +1,7 @@
+export interface Movie{
+
+id: string;
+primaryImage: {
+    url: string;
+  };
+}
---
+++ src/app/interfaces/User.ts
@@ -0,0 +1,10 @@
+export interface User{
+    id: number,
+    Name: string;
+    Email: string;
+    Password: string;
+    BirthDate: string;
+    Country: string;
+    FavMovies: Array<any>;
+
+}
